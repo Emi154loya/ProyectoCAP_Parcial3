ee35d59204858686cc0f78597cadc7d12e6b5a1f
Victor Daniel Chavez Medina <victortyburon1212@gmail.com>
2022-11-20 12:21:17 -0700

    Creación del menú
    Se creó el menú para las formulas a calcular

--- main.cpp
+++ main.cpp
@@ -5,5 +5,11 @@ using namespace std;
 
 int main()
 {
+    cout << "================================================================" << endl;
+    cout << "============ Bienvenido a la Calculadora Formularia ============" << endl;
+    cout << "================================================================" << endl;
+    cout << "Escoge una opcion del menu" << endl;
+    cout << "[1] Formula general" << endl;
+    cout << "[2] " << endl;
     return 0;
 }
