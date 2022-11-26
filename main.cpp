1b86c990259f9570b647d23e38377fe883ebeb88
Emi154loya <emi154loya@gmail.com>
2022-11-26 16:33:34 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-// Funciones y procedimientos
+// --Funciones y procedimientos
 // Algebra
 void formulaGral();
 // Geometria
@@ -29,61 +29,82 @@ void volumenCasqueteApotema();
 void volumenCasqueteEsfera();
 // Aritmetica
 void aritmeticaFracciones();
+// Fisica
+void movimientoRectilineoUniforme();
+void movimientoRectilineoUniformementeAcelerado();
+void velocidadMovimientoCircularUniforme();
+void velocidadAngularMovimientoCircularUniforme();
+void periodoMovimientoCircularUniforme();
+void aceleracionCentripetaMovimientoCircularUniforme();
+void tiroParabolico();
+void fuerza();
+void peso();
+void friccion();
+void fuerzaElastica();
+void trabajo();
+void energiaCinetica();
+void energiaPotencial();
+void energiaPotencialElastica();
+void potencia();
 
 struct Fraccion
 {
-    int numerador=0, denominador=0;
+    int numerador = 0, denominador = 0;
 
-    Fraccion(int N, int D){
-        numerador=N;
-        denominador=D;
+    Fraccion(int N, int D)
+    {
+        numerador = N;
+        denominador = D;
     }
 
-    Fraccion operator+(Fraccion a){
+    Fraccion operator+(Fraccion a)
+    {
         Fraccion resultado(0, 0);
-        if (denominador==0 || a.denominador==0)
+        if (denominador == 0 || a.denominador == 0)
         {
             throw invalid_argument("!!! ERROR: El denominador no puede ser 0 !!!");
         }
         if (denominador == a.denominador)
         {
-            resultado.numerador = numerador+a.numerador;
+            resultado.numerador = numerador + a.numerador;
             resultado.denominador = denominador;
         }
         else
         {
             resultado.numerador = (numerador * a.denominador) + (a.numerador * denominador);
-			resultado.denominador = denominador * a.denominador;
+            resultado.denominador = denominador * a.denominador;
         }
         return resultado;
     }
 
-    Fraccion operator-(Fraccion a){
+    Fraccion operator-(Fraccion a)
+    {
         Fraccion resultado(0, 0);
-        if (denominador==0 || a.denominador==0)
+        if (denominador == 0 || a.denominador == 0)
         {
             throw invalid_argument("!!! ERROR: El denominador no puede ser 0 !!!");
         }
         if (denominador == a.denominador)
         {
-            resultado.numerador = numerador-a.numerador;
+            resultado.numerador = numerador - a.numerador;
             resultado.denominador = denominador;
         }
         else
         {
             resultado.numerador = (numerador * a.denominador) - (a.numerador * denominador);
-			resultado.denominador = denominador * a.denominador;
+            resultado.denominador = denominador * a.denominador;
         }
         return resultado;
     }
 
-    Fraccion operator/(Fraccion a){
+    Fraccion operator/(Fraccion a)
+    {
         Fraccion resultado(0, 0);
 
         resultado.numerador = numerador * a.denominador;
-		resultado.denominador = denominador * a.numerador;
+        resultado.denominador = denominador * a.numerador;
 
-        if (resultado.denominador==0)
+        if (resultado.denominador == 0)
         {
             throw invalid_argument("!!! ERROR: El denominador resultante es cero !!!");
         }
@@ -91,35 +112,35 @@ struct Fraccion
         return resultado;
     }
 
-    Fraccion operator*(Fraccion a){
+    Fraccion operator*(Fraccion a)
+    {
         Fraccion resultado(0, 0);
-        if (denominador==0 || a.denominador==0)
+        if (denominador == 0 || a.denominador == 0)
         {
             throw invalid_argument("!!! ERROR: El denominador no puede ser 0 !!!");
         }
 
         resultado.numerador = numerador * a.numerador;
-		resultado.denominador = denominador * a.denominador;
+        resultado.denominador = denominador * a.denominador;
 
         return resultado;
     }
 
-    //Funciones friend para sobrecargar los operadores stream    
-    friend std::ostream& operator <<(std::ostream &os,Fraccion &obj){
-        os<<obj.numerador<<" / "<<obj.denominador<<endl;
+    // Funciones friend para sobrecargar los operadores stream
+    friend std::ostream &operator<<(std::ostream &os, Fraccion &obj)
+    {
+        os << obj.numerador << " / " << obj.denominador << endl;
         return os;
     }
-    void print() { cout << numerador << " / "<<denominador<< endl; }
-
+    void print() { cout << numerador << " / " << denominador << endl; }
 };
-
 
 int main()
 {
-    int choice=0;
-    int choice2=0;
+    int choice = 0;
+    int choice2 = 0;
+    int choice3 = 0;
 
-    
     // do while para ciclar el menu
     do
     {
@@ -130,7 +151,8 @@ int main()
         cout << "[1] Algebra" << endl;
         cout << "[2] Geometria" << endl;
         cout << "[3] Aritmetica" << endl;
-        cout << "[4] Salir" << endl;
+        cout << "[4] Fisica" << endl;
+        cout << "[5] Salir" << endl;
         cin >> choice;
 
         // Switch para desplegar segundo menu
@@ -148,20 +170,20 @@ int main()
                 {
                     formulaGral();
                 }
-                catch(invalid_argument &e)
+                catch (invalid_argument &e)
                 {
                     cout << e.what() << endl;
                 }
-                
+
                 break;
-            
+
             default:
                 break;
             }
             break;
 
         case 2:
-           cout << "====== Formulas de geometria ======" << endl;
+            cout << "====== Formulas de geometria ======" << endl;
             cout << "[1] Volumen de un cono" << endl;
             cout << "[2] Volumen de una esfera" << endl;
             cout << "[3] Volumen de una piramide cuadrada" << endl;
@@ -252,7 +274,90 @@ int main()
             case 1:
                 aritmeticaFracciones();
                 break;
-            
+
+            default:
+                break;
+            }
+            break;
+        case 4:
+            cout << "====== Formulas de Fisica ======" << endl;
+            cout << "[1]  Movimiento rectilineo uniforme" << endl;
+            cout << "[2]  Movimiento rectilineo uniformemente acelerado" << endl;
+            cout << "[3]  Movimiento circular uniforme" << endl;
+            cout << "[4]  Tiro parabolico" << endl;
+            cout << "[5]  Fuerza" << endl;
+            cout << "[6]  Peso" << endl;
+            cout << "[7]  Friccion" << endl;
+            cout << "[8]  Fuerza elastica" << endl;
+            cout << "[9]  Trabajo" << endl;
+            cout << "[10] Energia cinetica" << endl;
+            cout << "[11] Energia potencial" << endl;
+            cout << "[12] Energia potencial elastica" << endl;
+            cout << "[13] Potencia" << endl;
+            cin >> choice2;
+            switch (choice2)
+            {
+            case 1:
+                movimientoRectilineoUniforme();
+                break;
+            case 2:
+                movimientoRectilineoUniformementeAcelerado();
+                break;
+            case 3:
+                cout << "====== Movimiento Circular Uniforme ======" << endl;
+                cout << "[1] Velocidad" << endl;
+                cout << "[2] Velocidad angular" << endl;
+                cout << "[3] Periodo" << endl;
+                cout << "[4] Aceleracion centripeta" << endl;
+                cin >> choice3;
+                switch (choice3)
+                {
+                case 1:
+                    velocidadMovimientoCircularUniforme();
+                    break;
+                case 2:
+                    velocidadAngularMovimientoCircularUniforme();
+                    break;
+                case 3:
+                    periodoMovimientoCircularUniforme();
+                    break;
+                case 4:
+                    aceleracionCentripetaMovimientoCircularUniforme();
+                    break;
+                default:
+                    break;
+                }
+                break;
+            case 4:
+                tiroParabolico();
+                break;
+            case 5:
+                fuerza();
+                break;
+            case 6:
+                peso();
+                break;
+            case 7:
+                friccion();
+                break;
+            case 8:
+                fuerzaElastica();
+                break;
+            case 9:
+                trabajo();
+                break;
+            case 10:
+                energiaCinetica();
+                break;
+            case 11:
+                energiaPotencial();
+                break;
+            case 12:
+                energiaPotencialElastica();
+                break;
+            case 13:
+                potencia();
+                break;
             default:
                 break;
             }
@@ -261,8 +366,8 @@ int main()
         default:
             break;
         }
-    } while (choice!=4);
-    
+    } while (choice != 5);
+
     return 0;
 }
 
@@ -273,8 +378,9 @@ int main()
 */
 
 // Formula General
-void formulaGral(){
-    double termCuadratico=0, termLineal=0, termIndepend=0, res1=0, res2=0, discriminante=0;
+void formulaGral()
+{
+    double termCuadratico = 0, termLineal = 0, termIndepend = 0, res1 = 0, res2 = 0, discriminante = 0;
 
     cout << "Introduce el coeficiente del termino cuadratico" << endl;
     cin >> termCuadratico;
@@ -282,18 +388,18 @@ void formulaGral(){
     cin >> termLineal;
     cout << "Introduce el coeficiente del termino independiente" << endl;
     cin >> termIndepend;
-    
-    discriminante=(pow(termLineal, 2))-(4*termCuadratico*termIndepend);
 
-    if (discriminante<0)
+    discriminante = (pow(termLineal, 2)) - (4 * termCuadratico * termIndepend);
+
+    if (discriminante < 0)
     {
         throw invalid_argument("!!! ERROR: El discriminante es negativo, no se puede resolver la ecuacion !!!");
     }
-    
+
     // Discriminante sumado
-    res1=((-1*termLineal)+(sqrt(discriminante)))/(2*termCuadratico);
+    res1 = ((-1 * termLineal) + (sqrt(discriminante))) / (2 * termCuadratico);
     // Discriminante restado
-    res2=((-1*termLineal)-(sqrt(discriminante)))/(2*termCuadratico);
+    res2 = ((-1 * termLineal) - (sqrt(discriminante))) / (2 * termCuadratico);
 
     // X sumado
     cout << "x+ = " << res1 << endl;
@@ -308,21 +414,23 @@ void formulaGral(){
 */
 
 // Volumen de un cono
-void volumenCono(){
-    double radioBase=0, altura=0, volumen=0;
+void volumenCono()
+{
+    double radioBase = 0, altura = 0, volumen = 0;
 
     cout << "Introduce el radio de la base" << endl;
     cin >> radioBase;
     cout << "Introduce la altura" << endl;
     cin >> altura;
-    volumen=((M_PI*(pow(radioBase, 2)*altura))/3);
+    volumen = ((M_PI * (pow(radioBase, 2) * altura)) / 3);
 
     cout << "El volumen es: " << volumen << endl;
 }
 
-//Volumen de un tronco de cono
-void volumenTroncoCono(){
-    double radioBase=0, radioBaseMenor=0, altura=0, volumen=0;
+// Volumen de un tronco de cono
+void volumenTroncoCono()
+{
+    double radioBase = 0, radioBaseMenor = 0, altura = 0, volumen = 0;
 
     cout << "Introduce el radio mayor del cono" << endl;
     cin >> radioBase;
@@ -330,84 +438,97 @@ void volumenTroncoCono(){
     cin >> radioBaseMenor;
     cout << "Introduce la altura del cono" << endl;
     cin >> altura;
-    volumen=((altura*M_PI*((pow(radioBase, 2))+(pow(radioBaseMenor, 2))+(radioBase*radioBaseMenor)))/3);
+    volumen = ((altura * M_PI * ((pow(radioBase, 2)) + (pow(radioBaseMenor, 2)) + (radioBase * radioBaseMenor))) / 3);
 
-    cout << "El volumen es: " << volumen << endl<< endl;
+    cout << "El volumen es: " << volumen << endl
+         << endl;
 }
 
 // Volumen de una esfera
-void volumenEsfera(){
-    double radio=0, volumen=0;
+void volumenEsfera()
+{
+    double radio = 0, volumen = 0;
 
     cout << "Introduce el radio de la esfera" << endl;
     cin >> radio;
-    volumen=((4*M_PI*(pow(radio, 3)))/3);
+    volumen = ((4 * M_PI * (pow(radio, 3))) / 3);
 
-    cout << "El volumen es: " << volumen << endl<< endl;
+    cout << "El volumen es: " << volumen << endl
+         << endl;
 }
 
 // Volumen de una Piramide Cuadrada
-void volumenPiramideCuadrada(){
-    double lado=0, altura=0, volumen=0;
+void volumenPiramideCuadrada()
+{
+    double lado = 0, altura = 0, volumen = 0;
 
     cout << "Introduce el lado de la base" << endl;
     cin >> lado;
     cout << "Introduce la altura" << endl;
     cin >> altura;
-    volumen=(((pow(lado, 2))*altura)/3);
+    volumen = (((pow(lado, 2)) * altura) / 3);
 
-    cout << "El volumen es: " << volumen << endl<< endl;
+    cout << "El volumen es: " << volumen << endl
+         << endl;
 }
 
 // Volumen de un Tetraedro Regular
-void volumenTetraedroRegular(){
-    double lado=0, volumen=0;
+void volumenTetraedroRegular()
+{
+    double lado = 0, volumen = 0;
 
     cout << "Introduce el lado de la base" << endl;
     cin >> lado;
-    volumen=((pow(lado,3))*((sqrt(2))/12));
+    volumen = ((pow(lado, 3)) * ((sqrt(2)) / 12));
 
-    cout << "El volumen es: " << volumen << endl<< endl;
+    cout << "El volumen es: " << volumen << endl
+         << endl;
 }
 
 // Volumen de un Tetraedro con base Regular
-void volumenTetraedroBaseRegular(){
-    double lado=0, altura=0, volumen=0;
+void volumenTetraedroBaseRegular()
+{
+    double lado = 0, altura = 0, volumen = 0;
 
     cout << "Introduce el lado de la base" << endl;
     cin >> lado;
     cout << "Introduce la altura del tetraedro" << endl;
     cin >> altura;
 
-    volumen=(altura*(pow(lado,2))*((sqrt(3))/12));
+    volumen = (altura * (pow(lado, 2)) * ((sqrt(3)) / 12));
 
-    cout << "El volumen es: " << volumen << endl<< endl;
+    cout << "El volumen es: " << volumen << endl
+         << endl;
 }
 
 // Volumen de un casquete usando apotema
-void volumenCasqueteApotema(){
-    double altura=0, apotema=0, volumen=0;
+void volumenCasqueteApotema()
+{
+    double altura = 0, apotema = 0, volumen = 0;
 
     cout << "Introduce el radio de la base del casquete (apotema)" << endl;
     cin >> apotema;
     cout << "Introduce la altura del casquete" << endl;
     cin >> altura;
-    volumen=(( (M_PI*altura) * ((3*(pow(apotema, 2)))+(pow(altura,2))))/6);
+    volumen = (((M_PI * altura) * ((3 * (pow(apotema, 2))) + (pow(altura, 2)))) / 6);
 
-    cout << "El volumen es: " << volumen << endl<< endl;
+    cout << "El volumen es: " << volumen << endl
+         << endl;
 }
 
-//Volumen de un casquete desde el radio de la esfera
-void volumenCasqueteEsfera(){
-    double altura=0, radio=0, volumen=0;
+// Volumen de un casquete desde el radio de la esfera
+void volumenCasqueteEsfera()
+{
+    double altura = 0, radio = 0, volumen = 0;
 
     cout << "Introduce el radio de la esfera" << endl;
     cin >> radio;
     cout << "Introduce la altura del casquete" << endl;
     cin >> altura;
-    volumen=(((M_PI*(pow(altura,2))) * ((3*radio)-altura))/3);
+    volumen = (((M_PI * (pow(altura, 2))) * ((3 * radio) - altura)) / 3);
 
-    cout << "El volumen es: " << volumen << endl<< endl;
+    cout << "El volumen es: " << volumen << endl
+         << endl;
 }
 
 /*
@@ -417,8 +538,9 @@ void volumenCasqueteEsfera(){
 */
 
 // Aritmetica de dos fracciones
-void aritmeticaFracciones(){
-    Fraccion fraccionA(0,0), fraccionB(0,0), resultado(0,0);
+void aritmeticaFracciones()
+{
+    Fraccion fraccionA(0, 0), fraccionB(0, 0), resultado(0, 0);
 
     cout << "Introduce el numerador de tu primera fraccion: ";
     cin >> fraccionA.numerador;
@@ -429,12 +551,95 @@ void aritmeticaFracciones(){
     cout << "Introduce el denominador de tu segunda fraccion: ";
     cin >> fraccionB.denominador;
 
-    resultado=fraccionA+fraccionB;
+    resultado = fraccionA + fraccionB;
     cout << "La suma es: " << resultado << endl;
-    resultado=fraccionA-fraccionB;
+    resultado = fraccionA - fraccionB;
     cout << "La resta es: " << resultado << endl;
-    resultado=fraccionA/fraccionB;
+    resultado = fraccionA / fraccionB;
     cout << "La division es: " << resultado << endl;
-    resultado=fraccionA*fraccionB;
+    resultado = fraccionA * fraccionB;
     cout << "La multiplicacion es: " << resultado << endl;
+}
+
+/*
+================
+***FISICA***
+================
+*/
+
+// Movimiento Rectilineo Uniforme
+void movimientoRectilineoUniforme()
+{
+    double posicionF = 0, posicionI = 0, velocidad = 0, tiempo = 0;
+
+    cout << "Introduce la posicion inicial (en m): ";
+    cin >> posicionI;
+    cout << "Introduce la velocidad (en m/s):";
+    cin >> velocidad;
+    cout << "Introduce el tiempo (en s): ";
+    cin >> tiempo;
+
+    posicionF = posicionI + (velocidad * tiempo);
+    cout << "La posicion final es: " << posicionF << "m" << endl;
+}
+
+void movimientoRectilineoUniformementeAcelerado()
+{
+    double posicionF = 0, velocidadF = 0, posicionI = 0, velocidadI = 0, aceleracion = 0, tiempo = 0;
+    cout << "Introduce la posicion inicial (en m): ";
+    cin >> posicionI;
+    cout << "Introduce la velocidad inicial (en m/s): ";
+    cin >> velocidadI;
+    cout << "Introduce la aceleracion (en m/s^2): ";
+    cin >> aceleracion;
+    cout << "Introduce el tiempo (en s): ";
+    cin >> tiempo;
+
+    posicionF = posicionI + (velocidadI * tiempo) + ((aceleracion * pow(tiempo, 2)) / 2);
+    velocidadF = velocidadI + (aceleracion * tiempo);
+    cout << "La posicion final es: " << posicionF << "m" << endl;
+    cout << "La velocidad final es: " << velocidadF << "m/s" << endl;
+}
+
+void velocidadMovimientoCircularUniforme()
+{
+}
+void velocidadAngularMovimientoCircularUniforme()
+{
+}
+void periodoMovimientoCircularUniforme()
+{
+}
+void aceleracionCentripetaMovimientoCircularUniforme()
+{
+}
+void tiroParabolico()
+{
+}
+void fuerza()
+{
+}
+void peso()
+{
+}
+void friccion()
+{
+}
+void fuerzaElastica()
+{
+}
+void trabajo()
+{
+}
+void energiaCinetica()
+{
+}
+void energiaPotencial()
+{
+}
+void energiaPotencialElastica()
+{
+}
+void potencia()
+{
 }
